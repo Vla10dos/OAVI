16ba62820207aac8918b5d438ac7d08bb3d59435
Vlad Spasskiy <spassvlad@mail.ru>
2025-05-30 10:29:34 +0300

    commit10

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -76,9 +76,9 @@ image = load_image(image_path)
 
 split_rgb(image)
 
-rgb_to_hsi(image)
+rgb_to_hsiii(image)
 
-invert_intenффsity(image)
+invert_intensity(image)
 
 save_image(nearest_neighbor_resize(image, 2), "Interpolation.png")
 
